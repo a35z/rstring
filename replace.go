6f4171d40eb3218b793297ec59faa14d871765d2
Ito <112294447+a35z@users.noreply.github.com>
2022-08-28 11:58:35 +0800

    Merged

--- replace.go
+++ replace.go
@@ -3,6 +3,7 @@ package rstring
 import (
 	"sort"
 	"strings"
+	"unicode"
 )
 
 func ReplaceAllLike(s string, rm map[string]string) string {
@@ -54,4 +55,42 @@ func mockString(src, dest string) string {
 	} else {
 		return dest
 	}
+}
+
+func isAllUpper(s string) bool {
+	for _, r := range s {
+		if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func isAllLower(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLower(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func isAllTitle(s string) bool {
+	for i, r := range s {
+		if !unicode.IsLetter(r) {
+			continue
+		}
+		if i == 0 && !unicode.IsUpper(r) {
+			return false
+		}
+		if i > 0 && !unicode.IsLower(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func indexWithoutCase(s string, substr string) int {
+	s, substr = strings.ToUpper(s), strings.ToUpper(substr)
+	return strings.Index(s, substr)
 }
